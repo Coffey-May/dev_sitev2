c2032e83582bc04a6bfb67aca6745c71de386c0d
Coffey May <jcoffeymay6@gmail.com>
2020-11-26 20:19:26 -0600

    m

--- src/components/Home2.js
+++ src/components/Home2.js
@@ -24,7 +24,7 @@ export default function Home2() {
                 <div id=" main-content2" style={{ border: '1px solid black', height: '80vh' }} className="content-wrapper2">
                     <h3>ABOUT ME</h3>
                     <div style={{ border: '1px solid black', padding: '1em', width: '70vw', height: '65vh', margin: '0 auto', backgroundColor: 'rgba(255,255,255,0.5)' }}>
-                        <p style={{ fontSize: '4vw' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste fugiat quas exercitationem explicabo architecto eligendi sint, ex omnis dolor est mollitia numquam atque id odio dicta eos illum aliquid accusamus pariatur nostrum! Maiores non voluptas at alias doloremque nulla, asperiores nesciunt dicta doloribus rem, neque sint obcaecati repudiandae. Repellat, quibusdam?</p>
+                        <p style={{ fontSize: '2.5vw' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste fugiat quas exercitationem explicabo architecto eligendi sint, ex omnis dolor est mollitia numquam atque id odio dicta eos illum aliquid accusamus pariatur nostrum! Maiores non voluptas at alias doloremque nulla, asperiores nesciunt dicta doloribus rem, neque sint obcaecati repudiandae. Repellat, quibusdam?</p>
 
 
                     </div>
