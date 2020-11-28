9a0645b06c7970667abf240ae90eef49d11dc93e
Coffey May <jcoffeymay6@gmail.com>
2020-11-28 01:45:11 -0600

    parse api data

--- src/components/WeatherAPI.js
+++ src/components/WeatherAPI.js
@@ -1,48 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 require('dotenv').config();
 console.log(process.env)
 const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
-export function fetchData() {
+
+export default function WeatherAPI() {
+    useEffect(() => {
+        // Good!
+        fetchData()
+    }, []);
+
+    const [cityName, setCityName] = useState("London");
+    function fetchData() {
+
+        // var cityName = "Nashville"
 
 
 
-    var city_name = "Nashville"
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${REACT_APP_API_KEY}`).then(response => {
+            return response.json()
+        }).then(data => {
+            console.log(data)
+            const num = (data.main.temp - 273.15) * 1.8000 + 32.00;
+            const cities = document.getElementsByTagName('option');
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${REACT_APP_API_KEY}`).then(response => {
-        return response.json()
-    }).then(data => {
-        console.log(data)
-        const num = (data.main.temp - 273.15) * 1.8000 + 32.00;
-        const cities = document.getElementsByTagName('option');
+            var citiesArr = cities;
 
+            setTimeout(function () {
+                [...citiesArr].forEach(function (element) {
+                    console.log(element.value)
+                    if (element.value === "London") {
+                        setCityName("London")
+                    }
+                });
+            }, 1000);
 
+            // document.getElementById('my-select').addEventListener('change', function () {
+            //     console.log('You selected: ', this.value);
+            // });
 
-        var citiesArr = cities;
-        // console.log(citiesArr[0]);
-        setTimeout(function () {
-            [...citiesArr].forEach(function (element) {
-                console.log(element.value)
-
-            });
-        }, 1000);
-
-        var n = num.toFixed(2);
-        let state;
-        if (data.name === 'Dallas') {
-            state = "TX"
-        } else if (data.name === "Nashville") {
-            state = "TN"
-        } else if (data.name === "Los Angeles") {
-            state = "CA"
-        } else if (data.name === "London") {
-            state = "UK"
-        } else if (data.name === "New York City") {
-            state = "NY"
-        }
-        document.getElementById('weather_card').innerHTML = `
+            var n = num.toFixed(2);
+            let state;
+            if (data.name === 'Dallas') {
+                state = "TX"
+            } else if (data.name === "Nashville") {
+                state = "TN"
+            } else if (data.name === "Los Angeles") {
+                state = "CA"
+            } else if (data.name === "London") {
+                state = "UK"
+            } else if (data.name === "New York City") {
+                state = "NY"
+            }
+            document.getElementById('weather_card').innerHTML = `
         <div style ="height:10em; width:8em; border:1px solid white; ">
         <label style="color:white;" for="city-select">Choose City:</label>
-        <select style="color:white;background-color: transparent;" name="cars" id="cars">
+        <select id="my-select" style="color:white;background-color: transparent;" name="cars" id="cars">
         <option class="select_city" style="background-color: transparent;" value="Select City">Select City</option>
         <option class="select_city" style="background-color: transparent;" value="Nashville">Nashville</option>
         <option class="select_city" style="background-color: transparent;" value="London">London</option>
@@ -56,13 +68,13 @@ export function fetchData() {
         <h6>${n} °F</h6>
         </div>
               `
-    })
-}
-fetchData()
+        })
+    }
 
 
 
-export default function WeatherAPI() {
+
+
 
 
 
