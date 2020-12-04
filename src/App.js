fe5f575e6341fdb090119d18cef7a85ffbeba278
Coffey May <jcoffeymay6@gmail.com>
2020-12-03 20:11:20 -0600

    began auth

--- src/App.js
+++ src/App.js
@@ -6,19 +6,32 @@ import Home from './components/Home';
 import Nav from './components/Nav';
 import Contact from './Contact';
 import Projects from './Projects';
+import { AuthProvider } from "./contexts/AuthContext";
+import Login from "./Login"
+import Signup from "./Signup"
+import PrivateRoute from "./PrivateRoute"
+import ForgotPassword from "./ForgotPassword"
+import UpdateProfile from "./UpdateProfile"
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Nav />
-        <Switch>
-          <Home path="/" exact component={Home} style={{ width: '80vw' }} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/contact" component={Contact} />
+      <AuthProvider>
+        <div className="App">
+          <Nav />
+          <Switch>
+            <Home path="/" exact component={Home} style={{ width: '80vw' }} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
 
-        </Switch>
-      </div>
+            <PrivateRoute path="/update-profile" component={UpdateProfile} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgot-password" component={ForgotPassword} />
+
+          </Switch>
+        </div>
+      </AuthProvider>
     </Router >
   );
 }
