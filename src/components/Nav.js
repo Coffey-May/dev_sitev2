defae8588c1ae3a55d0491efaa86758fc3ec3893
Coffey May <jcoffeymay6@gmail.com>
2020-12-05 17:47:50 -0600

    navbar responsive

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,11 +14,18 @@ import { Navbar } from 'react-bootstrap';
 const Nav = () => {
     const { currentUser } = useAuth()
     if (currentUser === null) {
-        var link =
+        var link = <>
             <NavLink to="/login" style={{ textDecoration: 'none', color: 'white' }} className="nav-link">
-                <li> <h5 style={{ fontFamily: 'sans-serif', padding: '2px' }} >Login </h5></li>
-
+                {/* <li class="glyphicon glyphicon-log-in" style={{ fontFamily: 'sans-serif', padding: '2px' }} >Login </li> */}
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
             </NavLink>
+
+
+            <NavLink to="/signup" style={{ textDecoration: 'none', color: 'white' }} className="nav-link">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            </NavLink>
+        </>
+
     } else if (currentUser != null) {
         link = <Dashboard />
 
@@ -32,19 +39,19 @@ const Nav = () => {
         <nav id="page1" className="Nav navbar navbar-inverse" id="Nav" style={{ margin: '0', borderRadius: '0' }} >
             <div style={{ color: 'white' }} class="container-fluid">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <button style={{ marginTop: '2.5vh' }} type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                         <span style={{ color: 'white' }} class="icon-bar"></span>
                         <span style={{ color: 'white' }} class="icon-bar"></span>
                         <span style={{ color: 'white' }} class="icon-bar"></span>
                     </button>
 
-                    <div style={{ display: "flex", flexDirection: 'row' }}>
+                    <div style={{ paddingLeft: '2vw', marginTop: '2.5vh', display: "flex", flexDirection: 'row' }}>
                         <p style={{ fontFamily: 'sans-serif', border: '2px solid white', padding: '2px' }} >CM </p>
                         <p style={{ marginTop: '.25vh', fontFamily: 'sans-serif', padding: '2px' }}> &nbsp;&nbsp; Full Stack Software Developer</p>
                     </div>
                 </div>
                 <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav" style={{ width: '70vw', listStyle: 'none', justifyContent: 'space-between' }}>
+                    <ul class="nav navbar-nav" style={{ marginTop: '2.5vh', width: '60vw', listStyle: 'none', justifyContent: 'space-between' }}>
                         {/* <div style={{ display: "flex", flexDirection: 'row', width: '35vw', justifyContent: 'space-between' }}> */}
                         <NavLink to="/" style={{ textDecoration: 'none', color: 'white' }} className="nav-link" >
 
