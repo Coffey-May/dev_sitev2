e0f440564621c0a7d4b8dbdd5316efcdbdcc0090
Coffey May <jcoffeymay6@gmail.com>
2020-12-05 01:28:49 -0600

    navlinks and routing fine tuned

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -22,16 +22,7 @@ export default function Dashboard() {
 
     return (
         <>
-            {/* <Card>
-                <Card.Body> */}
-            {/* <h2 className="text-center mb-4">Profile</h2>
-                    {error && <Alert variant="danger">{error}</Alert>} */}
-            {/* <strong>Email:</strong> {currentUser.email} */}
-            {/* <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-                        Update Profile
-          </Link> */}
-            {/* </Card.Body>
-            </Card> */}
+
             <div className="w-100 text-center mt-2">
                 {/* <Button variant="link" onClick={handleLogout}>
                     Log Out
@@ -39,11 +30,21 @@ export default function Dashboard() {
         </Button> */}
 
 
-                {/* <NavLink onClick={handleLogout} style={{ textDecoration: 'none', color: 'white' }} > */}
+
                 <h5 onClick={handleLogout} style={{ cursor: 'pointer', color: 'white', fontFamily: 'sans-serif', padding: '2px' }} >Log Out </h5>
 
-                {/* </NavLink> */}
+
             </div>
+            {/* <Card>
+                <Card.Body> */}
+            {/* <h5 style={{ cursor: 'pointer', color: 'white', fontFamily: 'sans-serif', padding: '2px' }} className="text-center mb-4">Profile</h5> */}
+            {/* {error && <Alert variant="danger">{error}</Alert>} */}
+            {/* <strong>Email:</strong> {currentUser.email} */}
+            <Link style={{ cursor: 'pointer', color: 'white', fontFamily: 'sans-serif', padding: '2px' }} to="/update-profile" className="btn btn-primary w-100 mt-3">
+                Profile
+          </Link>
+            {/* </Card.Body>
+            </Card> */}
         </>
     )
 }
