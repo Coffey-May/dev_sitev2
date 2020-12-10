d32b24e5d1d11fd172ca16da9fd76d928af73ece
Coffey May <jcoffeymay6@gmail.com>
2020-12-09 21:20:38 -0600

    portfolio images dynamicaally rendering

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import HomeSlides from './components/HomeSlides';
+import HomeSlides from './components/HomeSlides';
 import { FaCopyright } from 'react-icons/fa';
 
 export default function Projects() {
@@ -7,24 +7,10 @@ export default function Projects() {
         <>
             <div id="colorBG">
                 <div style={{ height: '80vh' }} >
-                    {/* <div style={{ height: '80vh', width: '20vw', padding: '1em' }}>
-                                <div id="projects-div-main" style={{ height: '76vh', width: '18vw', padding: '1em' }} >kjdbkjsbdvk</div>
-                            </div> */}
-                    <div>
-                        <div style={{ margin: '0 auto', height: '50vh', width: '80vw', padding: '1em' }}>
-                            <div id="projects-div-main" style={{ height: '48vh', width: '76vw', padding: '1em' }}>
-                                <h1>jdsnbjkvkn</h1>
-                            </div>
-                        </div>
-                        <div style={{ margin: '0 auto', height: '30vh', width: '80vw', padding: '1em' }}>
-                            <div id="projects-div-main" style={{ height: '26vh', width: '76vw', padding: '1em' }}>
-                                <h1>jdsnbjkvkn</h1>
-                            </div>
-                        </div>
-                    </div>
+
 
                     {/* <h1>Hello</h1> */}
-                    {/* <HomeSlides /> */}
+                    <HomeSlides />
                 </div>
             </div>
             <footer id="footer2" style={{ border: '1px solid black', height: '10vh', backgroundColor: 'black', color: 'white' }}>
--- src/components/HomeSlides.js
+++ src/components/HomeSlides.js
@@ -2,37 +2,49 @@ import React from 'react';
 import Slider from 'react-animated-slider';
 import WeatherComponent from "./WeatherComponent"
 import { NavLink } from "react-router-dom";
+import 'react-animated-slider/build/horizontal.css';
 
-// import './App.css';
-
-// import 'react-animated-slider/build/horizontal.css';
+import Pic from './IMG_20201206_153507.jpg'
 
 
-// const slides = [
-//     { title: 'Painting Gallery', image: '', description: 'This Project was built using ReactJS. It showcases my oil paintings by dynamically populating a list of cards with detail modals.  This project also impliments NodeJS, Routing, and Several NPM packages to achieve Parallax effect, and UI composition.', link: 'http://coffeymayart.com/' },
-//     { title: 'Second item', image: '', description: 'Lorem 1ipsum', link: "poop" }
 
-// ];
+
+let pic1 = Pic
+let link1 = '${<NavLink to={/chat}>Enter Chat App</NavLink >}'
+
+
+
+
+const slides = [
+    { title: 'Painting Gallery', image: `${pic1}`, description: 'This Project was built using ReactJS. It showcases my oil paintings by dynamically populating a list of cards with detail modals.  This project also impliments NodeJS, Routing, and Several NPM packages to achieve Parallax effect, and UI composition.', link: 'http://coffeymayart.com/' },
+    { title: 'Comment secton', image: `${pic1}`, description: 'This feature allows a logged in User the affordances to perform full CRUD operations on a social media style comment thread ', link: `${link1}` },
+    { title: 'Comment secton', image: `${pic1}`, description: 'This feature allows a logged in User the affordances to perform full CRUD operations on a social media style comment thread ', link: `${link1}` }
+
+];
 
 export default function HomeSlides() {
     return (
-        <div id="projectBG" style={{ height: '80vh', width: '100vw', margin: '0 auto' }}>
+        <div id="colorBG" style={{ height: '80vh', width: '100vw', margin: '0 auto' }}>
 
             {/* <WeatherComponent /> */}
             {/* <h1>Hello</h1> */}
-            {/* 
-            <Slider speed={300}>
-                {slides.map((slide, index) => <div key={index}>
+            {/* <img style={{ height: '4em', width: '4em' }} src={pic1} alt="" /> */}
+
+            <Slider speed={1500}>
+                {slides.map((slide, index) => <div style={{ height: '80vh', color: 'white', padding: '2em' }} key={index}>
+
                     <h2 style={{ marginTop: '10vh' }}>{slide.title}</h2>
-                    <div>{slide.image}</div>
-                    <div>{slide.description}</div>
-                    <h4> <a style={{ height: '20px' }} href={slide.link}>coffeymayart.com</a> </h4>
+                    <img style={{ height: '4em', width: '4em' }} src={slide.image} alt="" />
+                    {/* <div>{slide.image}</div> */}
+                    <div style={{ width: '65vw', margin: '0 auto' }}> <h3>{slide.description}</h3> </div>
+                    <a style={{ height: '20px' }} href={slide.link}> {slide.link}</a>
+
 
 
 
 
                 </div>)}
-            </Slider> */}
+            </Slider>
         </div>
     )
 }
