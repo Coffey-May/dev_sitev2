55cd6a437854d443e8dcac83b8267bcfad444b52
Coffey May <jcoffeymay6@gmail.com>
2020-12-10 17:46:00 -0600

    began projects section

--- src/components/HomeSlides.js
+++ src/components/HomeSlides.js
@@ -3,26 +3,31 @@ import Slider from 'react-animated-slider';
 import WeatherComponent from "./WeatherComponent"
 import { NavLink } from "react-router-dom";
 import 'react-animated-slider/build/horizontal.css';
+import { Link } from "react-router-dom";
 
-import Pic from './IMG_20201206_153507.jpg'
+import Pic from './Screen Shot 2020-12-10 at 4.40.52 PM.png'
+import Pic2 from './Screen Shot 2020-12-10 at 4.41.07 PM.png'
+import Pic3 from './Screen Shot 2020-12-10 at 4.41.27 PM.png'
 
 
 
+let link1 = '/weather'
 
 let pic1 = Pic
-let link1 = '${<NavLink to={/chat}>Enter Chat App</NavLink >}'
-
+let pic2 = Pic2
+let pic3 = Pic3
 
 
 
 const slides = [
-    { title: 'Painting Gallery', image: `${pic1}`, description: 'This Project was built using ReactJS. It showcases my oil paintings by dynamically populating a list of cards with detail modals.  This project also impliments NodeJS, Routing, and Several NPM packages to achieve Parallax effect, and UI composition.', link: 'http://coffeymayart.com/' },
-    { title: 'Comment secton', image: `${pic1}`, description: 'This feature allows a logged in User the affordances to perform full CRUD operations on a social media style comment thread ', link: `${link1}` },
-    { title: 'Comment secton', image: `${pic1}`, description: 'This feature allows a logged in User the affordances to perform full CRUD operations on a social media style comment thread ', link: `${link1}` }
+    { title: 'Painting Gallery', image: `${pic1}`, image2: `${pic2}`, image3: `${pic3}`, description: 'This Project was built using ReactJS. It showcases my oil paintings by dynamically populating a list of cards with detail modals.  This project also impliments NodeJS, Routing, and Several NPM packages to achieve Parallax effect, and UI composition.', link: 'http://coffeymayart.com/' },
+    { title: 'Comment secton', image: `${pic1}`, image2: `${pic1}`, image3: `${pic1}`, description: 'This feature allows a logged in User the affordances to perform full CRUD operations on a social media style comment thread ', link: `${link1}` },
+    { title: 'Comment secton', image: `${pic1}`, image2: `${pic1}`, image3: `${pic1}`, description: 'This feature allows a logged in User the affordances to perform full CRUD operations on a social media style comment thread ', link: `${link1}` }
 
 ];
 
 export default function HomeSlides() {
+
     return (
         <div id="colorBG" style={{ height: '80vh', width: '100vw', margin: '0 auto' }}>
 
@@ -31,15 +36,21 @@ export default function HomeSlides() {
             {/* <img style={{ height: '4em', width: '4em' }} src={pic1} alt="" /> */}
 
             <Slider speed={1500}>
-                {slides.map((slide, index) => <div style={{ height: '80vh', color: 'white', padding: '2em' }} key={index}>
+                {slides.map((slide, index) => <div style={{ overflow: 'scroll', height: '80vh', color: 'white', padding: '2em' }} key={index}>
 
-                    <h2 style={{ marginTop: '10vh' }}>{slide.title}</h2>
-                    <img style={{ height: '10em', width: '10em', backgroundSize: 'cover' }} src={slide.image} alt="" />
-                    {/* <div>{slide.image}</div> */}
-                    <div style={{ width: '65vw', margin: '0 auto' }}> <h3>{slide.description}</h3> </div>
+                    <h2 style={{ marginTop: '0vh' }}>{slide.title}</h2>
+
+                    <img id="project-pic" style={{ height: '15em', width: '25em', margin: '1em' }} src={slide.image} alt="image of art website" />
+                    <img id="project-pic" style={{ height: '15em', width: '25em', margin: '1em' }} src={slide.image2} alt="image of art website" />
+                    <img id="project-pic" style={{ height: '15em', width: '25em', margin: '1em' }} src={slide.image3} alt="image of art website" />
+                    <div style={{ width: '65vw', margin: '0 auto' }}>
+                        <h3>{slide.description}</h3>
+                    </div>
+
+
                     <h3>
                         <button>
-                            <a style={{ color: 'black', height: '20px' }} href={slide.link}>View</a>
+                            <a target="blank" style={{ color: 'black', height: '20px' }} href={slide.link}>View</a>
                         </button>
                     </h3>
 
