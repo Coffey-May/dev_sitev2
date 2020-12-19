4ad4df69bbb22920ed3b3e818220d80cfe9cfdf8
Coffey May <jcoffeymay6@gmail.com>
2020-12-19 01:06:47 -0600

    wethercomponent functionality complete

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,38 +1,46 @@
 import React from "react";
-import Pic from './cloudygif.gif'
+import { AuthProvider } from "../contexts/AuthContext";
+import Pic from './weathergif.gif'
 import Pic2 from './Screen Shot 2020-12-10 at 4.41.07 PM.png'
 import Pic3 from './Screen Shot 2020-12-10 at 4.41.27 PM.png'
 
 
 const Weather = props => (
-    <div className="weather__info">
-        {
-            props.city && props.country && <h2 className="weather__key"> Location:
+    <div style={{ display: "flex", FlexDirection: 'row', flexWrap: 'wrap', overflow: '  scroll' }} className="weather__info">
+        <div style={{ margin: '2em' }}>
+            <h2 style={{ margin: '0 auto' }}>Currently</h2>
+            <hr />
+            {
+                props.city && props.country && <h2 style={{ margin: '0 auto' }} className="weather__key"> Location:
 	 		<span className="weather__value"> {props.city}, {props.country}</span>
-            </h2>
-        }
-        {
-            props.temperature && <h3 className="weather__key"> Temperature:
+                </h2>
+            }
+            {
+                props.temperature && <h3 style={{ margin: '0 auto' }} className="weather__key"> Temperature:
 	 		<span className="weather__value"> {props.temperature} °F	</span>
-            </h3>
-        }
+                </h3>
+            }
 
-        {
-            props.humidity && <h3 className="weather__key"> Humidity:
+            {
+                props.humidity && <h3 style={{ margin: '0 auto' }} className="weather__key"> Humidity:
 	 		<span className="weather__value"> {props.humidity}% </span>
-            </h3>
-        }
-        {
-            <div className="sun-2-container">
-                {/* <img src={Pic} alt="" /> */}
-                <div className="sun2"></div>
-            </div>
-        }
-        {
-            props.description && <h2 className="weather__key"> Conditions:
+                </h3>
+            }
+
+            {
+                props.description && <h2 style={{ margin: '0 auto' }} className="weather__key"> Conditions:
 	 		<span className="weather__value"> {props.description} </span>
-            </h2>
-        }
+                </h2>
+            }
+        </div>
+        <div>
+            {
+                <div className="sun-2-container">
+                    <img className="weather-pic" style={{ padding: '2em', margin: '.5em', width: '40vw' }} src={Pic} alt="" />
+                    {/* <div className="sun2"></div> */}
+                </div>
+            }
+        </div>
 
         {
             props.error && <p className="weather__error">{props.error}</p>
