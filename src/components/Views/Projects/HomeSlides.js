342987660186fd3f92e3b2594ca4fc6be93433bb
Coffey May <jcoffeymay6@gmail.com>
2021-03-12 20:09:06 -0600

    pre-deploy commit

--- src/components/Views/Projects/HomeSlides.js
+++ src/components/Views/Projects/HomeSlides.js
@@ -16,7 +16,7 @@ import pic12 from '../images/Screen Shot 2021-03-11 at 11.53.32 AM.png';
 
 //Projects Slider and controls
 
-const imgStyle = { border: '13px solid #f5fff8', height: '15em', width: '23em', margin: '0 auto', boxShadow: ' 5px 10px 18px #0b0e29' }
+const imgStyle = { border: '13px solid #f5fff8', height: '15em', width: '23em', margin: '1rem', boxShadow: ' 5px 10px 18px #0b0e29' }
 let link1 = '/weather'
 let link2 = '/todo'
 
