9192eef932f743d20b890d9b7f789348df0898ca
Coffey May <jcoffeymay6@gmail.com>
2021-03-12 21:54:52 -0600

    pre-deploy commit

--- src/components/Views/Projects/HomeSlides.js
+++ src/components/Views/Projects/HomeSlides.js
@@ -16,7 +16,7 @@ import pic12 from '../images/Screen Shot 2021-03-11 at 11.53.32 AM.png';
 
 //Projects Slider and controls
 
-const imgStyle = { backgroundImage: 'contain', border: '13px solid #f5fff8', height: 'auto', width: '33vw', margin: '1rem', boxShadow: ' 5px 10px 18px #0b0e29' }
+const imgStyle = { backgroundImage: 'contain', border: '5px solid #f5fff8', height: 'auto', width: '33vw', margin: '1rem', boxShadow: ' 5px 10px 18px #0b0e29' }
 let link1 = '/weather'
 let link2 = '/todo'
 
@@ -32,12 +32,12 @@ export default function HomeSlides() {
         <div id="colorBG" style={{ height: '80vh', width: '100vw', margin: '0 auto' }}>
             <Slider speed={1500}>
                 {slides.map((slide, index) => <div style={{ overflow: 'scroll', height: '80vh', color: 'white', padding: '2em' }} key={index}>
-                    <h2 style={{ marginTop: '0vh', fontSize: '2em' }}>{slide.title}</h2>
+                    <h2 style={{ marginTop: '0vh' }}>{slide.title}</h2>
                     <img id="project-pic" style={imgStyle} src={slide.image} alt="screenshot of computer application" />
                     <img id="project-pic" style={imgStyle} src={slide.image2} alt="screenshot of computer application" />
                     <img id="project-pic" style={imgStyle} src={slide.image3} alt="screenshot of computer application" />
                     <div style={{ width: '65vw', margin: '0 auto' }}>
-                        <h3>{slide.description}</h3>
+                        <h3 style={{ fontSize: '1em' }}>{slide.description}</h3>
                     </div>
                     <h6 className="btncontainer">
                         <p className="animated-word">
