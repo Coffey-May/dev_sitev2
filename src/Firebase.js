25f8d8e186ecac26a8ac88599b5b7e2044e595e2
Coffey May <jcoffeymay6@gmail.com>
2021-03-13 11:16:16 -0600

    pre-deploy commit

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,7 +3,7 @@ import "firebase/auth"
 require('dotenv').config();
 
 const app = firebase.initializeApp({
-    apiKey: 'AIzaSyCpTvx6QA_i6gRdm1siJm7XKQlFfY1uY6k',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
