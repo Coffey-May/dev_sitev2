98d54ad9261f3a5f8b91622436e6baab53085162
Coffey May <jcoffeymay6@gmail.com>
2021-05-24 00:08:29 -0500

    landingpage near perfection

--- src/components/Views/Home/HomeTitle.js
+++ src/components/Views/Home/HomeTitle.js
@@ -48,7 +48,7 @@ const HomeTitle = ({ footer }) => {
                 <div id="objective"  >
                     <div style={{ border: '9px solid goldenrod', padding: '1em', backgroundColor: 'rgba(0, 0, 0, 0.455)' }}>
                         <strong>
-                            <h1 style={{ margin: '0 auto ', color: '#F7D37A', letterSpacing: '.2em', fontSize: '2em' }}>What I do</h1>
+                            <h1 style={{ margin: '0 auto ', color: '#F7D37A', letterSpacing: '.2em', fontSize: '2em' }}> WHAT I DO  </h1>
                             <hr style={{ color: 'goldenrod', margin: '0 auto' }} />
                             <h5 style={{ color: '#F7D37A' }}>
                                 Do you like finding elegant and simple solutions for real time challanges? Is your development team looking for an enthusiatic junior developer to help your company grow in todays evolving tech landscape?
@@ -57,9 +57,7 @@ const HomeTitle = ({ footer }) => {
                                 and I hope you enjoy...
                     </h5>
                         </strong>
-                    </div><div >
-
-                        <div style={{ color: '#F7D37A', marginTop: '2vh', border: '2px solid goldenrod', padding: '1em', backgroundColor: 'rgba(0, 0, 0, 0.155)' }}>
+                        <div style={{ margin: '0 auto', color: '#F7D37A', marginTop: '2vh', border: '1px solid goldenrod', padding: '1em', backgroundColor: 'rgba(0, 0, 0, 0.155)' }}>
                             <em>
                                 <p style={{ lineHeight: '3em', fontWeight: 'bolder', margin: '0 auto', color: 'whitesmoke', fontWeight: 'lighter', fontSize: 'xx-small', lineHeight: '8px' }}>"Mathematical science shows what is. It is the language of unseen relations between things.
                  But to use and apply that language, we must be able fully to appreciate, to feel, to seize the unseen, the unconscious."</p>
@@ -68,12 +66,9 @@ const HomeTitle = ({ footer }) => {
                             <p style={{ color: '#FBE9C1', margin: '0 auto', fontSize: 'xx-small' }}> <a target="blank" href="https://en.wikipedia.org/wiki/Ada_Lovelace">~Ada Lovelace~</a> </p>
                         </div>
                     </div>
-
-                    {/* <hr className="animated" /> */}
-
-
                 </div>
             </div>
+
         </Parallax >
     )
 }
