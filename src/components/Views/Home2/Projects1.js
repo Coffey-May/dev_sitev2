7e4011d062b74902e581f1ee405d66cc14ed13a6
Coffey May <jcoffeymay6@gmail.com>
2021-06-24 22:39:34 -0500

    touch-ups

--- src/components/Views/Home2/Projects1.js
+++ src/components/Views/Home2/Projects1.js
@@ -27,7 +27,7 @@ const slides = [
     { title: 'MERN Stack', image: `${pic10}`, image2: `${pic11}`, image3: `${pic12}`, description: 'This Full stack application was built using NodeJS, and interacts with a MongoDB database.  State is managed Client side via Redux, after authentication via Google OAuth or local sign/signup is completed. This project performs crud opertations on posts made by users sharing travel locales or events. ', link: 'https://coffey-may-coffey-may-merntime.zeet.app/' },
     { title: 'Painting Gallery', image: `${pic1}`, image2: `${pic2}`, image3: `${pic3}`, description: 'This Project was built using ReactJS. It showcases my oil paintings by dynamically populating a list of cards with detail modals. This project also impliments Routing, and Several NPM packages to achieve Parallax effect, and UI composition.', link: 'http://coffeymayart.com/' },
     { title: 'To Do Organizer', image: `${pic7}`, image2: `${pic8}`, image3: `${pic9}`, description: 'This feature presents the user with a to do list application with full CRUD functionailty and use of local storage.', link: `${link2}` },
-    { title: 'What Should I Eat?', image: `${pic7}`, image2: `${pic8}`, image3: `${pic9}`, description: 'This project allows a user to consume a REST api containing recipe data, and the ability to search for recipes by keyword.', link: `${link3}` }
+    { title: 'What Should I Eat?', image: `${pic7}`, image2: `${pic8}`, image3: `${pic9}`, description: 'This project allows a user to consume a REST api containing recipe data, and the ability to search for recipes by keyword. Affordances for linking to original content provided dynamically.', link: `${link3}` }
 ];
 
 export const Projects1 = () => {
